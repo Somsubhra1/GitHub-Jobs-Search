7240ffab484a4b952afb5cda82aa1566fe97ec98
Somsubhra Das <das.somsubhra1@gmail.com>
2020-10-15 21:41:33 +0530

    job fetch reducer done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+import useFetchJobs from "./useFetchJobs";
+import { Container } from "react-bootstrap";
+import Job from "./Job";
 
 function App() {
+  const { jobs, loading, error } = useFetchJobs();
+
+  const [params, setParams] = useState({});
+  const [page, setPage] = useState(0);
+
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+    <Container>
+      {loading && <h1>Loading...</h1>}
+      {error && <h1>Error. Try Refreshing.</h1>}
+      <h1>{jobs.length}</h1>
+      {jobs.map((job) => {
+        return <Job key={job.id} job={job} />;
+      })}
+    </Container>
   );
 }
 
